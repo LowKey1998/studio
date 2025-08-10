b2ee8780c6b8832ffcd40dee770720f11bbc30fc
Gerald <geraldfxzambia@gmail.com>
2025-08-10 13:25:31 +0000

    useSearchParams() should be wrapped in a suspense boundary at page "/adm

--- src/app/admin/quizzes/builder/page.tsx
+++ src/app/admin/quizzes/builder/page.tsx
@@ -1,12 +1,22 @@
-
 'use client';
+import * as React from 'react';
 import QuizBuilder from '@/components/quiz-builder';
 import { useSearchParams } from 'next/navigation';
+import { Skeleton } from '@/components/ui/skeleton';
 
-export default function CreateQuizPage() {
+function CreateQuizPageContent() {
     const searchParams = useSearchParams();
     const courseId = searchParams.get('courseId');
     const semesterId = searchParams.get('semesterId');
 
     return <QuizBuilder courseId={courseId} semesterId={semesterId} />;
+}
+
+
+export default function CreateQuizPage() {
+    return (
+        <React.Suspense fallback={<Skeleton className="h-96 w-full" />}>
+            <CreateQuizPageContent />
+        </React.Suspense>
+    );
 }
