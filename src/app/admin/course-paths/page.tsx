de946004c389ca64454c3b79f6f73eaffb918fe8
hundredx <geraldphiri100x@gmail.com>
2025-08-21 12:41:45 +0000

    Semester should be edittable and deletable. Same should be possible for

--- src/app/admin/course-paths/page.tsx
+++ src/app/admin/course-paths/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 import * as React from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
@@ -406,6 +405,3 @@ function AvailableCoursesColumn({ courses, targetSemester, setTargetSemester, on
         </Card>
     )
 }
-
-
-    
