d3184760a8326f05c6f1f9ec69c94e6f9a54193d
Gerald <geraldfxzambia@gmail.com>
2025-08-23 14:36:40 +0000

    website is some times showing two topbars

--- src/app/admin/hr/layout.tsx
+++
@@ -1,8 +0,0 @@
-// This file is obsolete as HR menu items are now dynamically added to the main layout.
-// It can be deleted.
-
-import DashboardLayout from "@/components/layout/dashboard-layout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
